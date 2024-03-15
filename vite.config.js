57c975137f4064de0cfcfd2b8b5bff4f20d7b789
Lana-ALnouri <alnourilanamhd@gmail.com>
2024-03-15 23:34:19 +0300

    pro

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/TravelMultiPages-react-vite",
+  base: "/TravelMulti-react-vite",
   plugins: [react()],
 })
