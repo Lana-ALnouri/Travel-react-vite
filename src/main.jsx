b872aa28eadc7836c3c9f0b12815ad755186fedb
Lana-ALnouri <alnourilanamhd@gmail.com>
2024-03-27 18:10:38 +0300

    files

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter ,RouterProvider,createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home/Home.jsx'
 import About from './pages/About/About.jsx';
+
+import PackagesPage from './pages/PackagesPage/PackagesPage.jsx'
+import { BrowserRouter ,RouterProvider,createBrowserRouter } from 'react-router-dom'
 const router = createBrowserRouter(
   [
     {
